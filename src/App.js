89422c906fea987d3658c6012bb3b4d3b32917f9
BenTully <tl2077@BenTullys-MacBook-Pro.local>
2020-03-06 21:32:46 -0500

    change text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!! Dynamic? Yes!!
+          Hi there!! Dynamic? Yes!! (Feature branch)
         </p>
         <a
           className="App-link"
